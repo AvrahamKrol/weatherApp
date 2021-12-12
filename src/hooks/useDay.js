1251aaa82fcd9b0a54e88daabbe51106a66e65e9
Rastsislau Krol <avraam87@gmail.com>
2021-12-12 18:00:28 +0200

    fixed problem with rendering widgets with click

--- src/hooks/useDay.js
+++ src/hooks/useDay.js
@@ -1,8 +1,6 @@
 export const useDay = (days, id) => {
     if (days) {
-        // eslint-disable-next-line
-        console.log(days?.find((day) => day?.day === id), id);
-        const findDay = days?.find((day) => day.day === id) || days[ 0 ];
+        const findDay = days?.find((day) => day.id === id) || days[ 0 ];
 
         return findDay;
     }
